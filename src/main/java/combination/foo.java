5b5239b3eb13c9b86d1030a82e2d757cd9028a5b
GaneshSPatil <ganeshpl@thoughtworks.com>
2016-10-21 20:44:17 +0530

    Changed the combination type from string to state

--- src/main/java/combination/foo.java
+++ src/main/java/combination/foo.java
@@ -1,23 +1,26 @@
 package combination;
 
+import automata.entity.State;
+import automata.entity.States;
+
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 
 public class foo {
-    public static List<List<String>> combinations(List<String> set){
-        List<List<String>> combs = new ArrayList<List<String>>();
+    public static List<States> combinations(States set){
+        List<States> combs = new ArrayList<States>();
         for (int k = 0; k <= set.size(); k++) {
-            List<List<String>> k_combs = k_combinations(set, k);
-            for (List<String> k_comb : k_combs) {
+            List<States> k_combs = k_combinations(set, k);
+            for (States k_comb : k_combs) {
                 combs.add(k_comb);
             }
         }
         return combs;
     }
 
-    private static List<List<String>> k_combinations(final List<String> set, int k) {
-        List<List<String>> k_combs = new ArrayList<List<String>>();
+    private static List<States> k_combinations(final States set, int k) {
+        List<States> k_combs = new ArrayList<States>();
         if (k > set.size() || k <= 0) {
             return k_combs;
         }
@@ -28,33 +31,40 @@ public class foo {
         }
 
         if (k == 1) {
-            for (int i = 0; i < set.size(); i++) {
-                final int finalI = i;
-                k_combs.add(new ArrayList<String>(){{
-                    int index = finalI;
-                    add(set.get(index));}});
+            for (final State state : set) {
+                k_combs.add(new States(){{add(state);}});
             }
             return k_combs;
         }
+        final ArrayList<State> uniqueSet = new ArrayList<State>(set);
+        for (int i = 0; i < new HashSet<State>(uniqueSet).size() - k + 1; i++) {
+            final int finalI = i;
+            States head = new States(){{add(uniqueSet.subList(finalI, finalI + 1).get(0));}};;
+            States listOfStates = new States();
+            for (State state : uniqueSet.subList(i + 1, set.size())) {
+                listOfStates.add(state);
+            }
+            List<States> tailcombs = new ArrayList<States>(k_combinations(listOfStates, k - 1));
 
-        for (int i = 0; i < new HashSet<String>(set).size() - k + 1; i++) {
-            List<String> head = new ArrayList<String>(set.subList(i, i + 1));
-            List<List<String>> tailcombs = new ArrayList<List<String>>(k_combinations(set.subList(i + 1, set.size()), k - 1));
             for (int j = 0; j < tailcombs.size(); j++) {
-                head.addAll(new ArrayList<String>(tailcombs.get(j)));
-                k_combs.add(new ArrayList<String>(head));
-                head = new ArrayList<String>(set.subList(i, i + 1));
+                head.addAll(new ArrayList<State>(tailcombs.get(j)));
+                States states = new States();
+                for (State state : new ArrayList<State>(head)) {
+                    states.add(state);
+                }
+                k_combs.add(states);
+                head = new States(){{add(uniqueSet.subList(finalI, finalI + 1).get(0));}};
             }
         }
         return k_combs;
     }
 
     public static void main(String[] args) {
-        ArrayList<String> set = new ArrayList<String>();
-        set.add("1");
-        set.add("2");
-        set.add("3");
-        set.add("4");
-        System.out.println(combinations(set));
+        States set = new States();
+        set.add(new State("1"));
+        set.add(new State("2"));
+        set.add(new State("3"));
+//        set.add(new State("4"));
+        System.out.println(combinations(set).size());
     }
 }
