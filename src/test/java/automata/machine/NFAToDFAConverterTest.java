8a6cead14c5570ecd3dc0b8e77d825d0ce8e2d94
GaneshSPatil <ganeshpl@thoughtworks.com>
2016-10-22 18:49:25 +0530

    Verified NFA to DFA conversion with the classroomNFA example

--- src/test/java/automata/machine/NFAToDFAConverterTest.java
+++ src/test/java/automata/machine/NFAToDFAConverterTest.java
@@ -94,4 +94,91 @@ public class NFAToDFAConverterTest {
 
         assertThat(dfa, is(expectedDFA));
     }
+
+    @Test
+    public void classRoomNFA() throws Exception {
+        States states = new States();
+        states.add(new State("q1"));
+        states.add(new State("q2"));
+        states.add(new State("q3"));
+
+
+        Alphabets alphabets = new Alphabets();
+        alphabets.add(new Alphabet("*"));
+        alphabets.add(new Alphabet("a"));
+        alphabets.add(new Alphabet("b"));
+
+        Transitions transitions = new Transitions();
+        transitions.put(new State("q1"), new HashMap<Alphabet, States>(){
+            {put(new Alphabet("*"), new States(){{add(new State("q3"));}});}
+            {put(new Alphabet("b"), new States(){{add(new State("q2"));}});}
+        });
+        transitions.put(new State("q2"), new HashMap<Alphabet, States>(){
+            {put(new Alphabet("a"), new States(){
+                {add(new State("q2"));}
+                {add(new State("q3"));}
+            });}
+            {put(new Alphabet("b"), new States(){{add(new State("q3"));}});}
+        });
+        transitions.put(new State("q3"), new HashMap<Alphabet, States>(){
+            {put(new Alphabet("a"), new States(){{add(new State("q1"));}});}
+        });
+
+        State initialState = new State("q1");
+        States finalStates = new States();
+        finalStates.add(new State("q1"));
+
+        NFA nfa = new NFA(states, alphabets, transitions, initialState, finalStates);
+
+        DFA dfa = nfa.toDFA();
+
+        States expectedStates = new States();
+        expectedStates.add(new State(""));
+        expectedStates.add(new State("q2"));
+        expectedStates.add(new State("q3"));
+        expectedStates.add(new State("q1,q2,q3"));
+        expectedStates.add(new State("q1,q3"));
+        expectedStates.add(new State("q2,q3"));
+
+        States expectedFinalStates = new States();
+        expectedFinalStates.add(new State("q1,q2,q3"));
+        expectedFinalStates.add(new State("q1,q3"));
+
+        Alphabets expectedAlphabets = new Alphabets();
+        expectedAlphabets.add(new Alphabet("a"));
+        expectedAlphabets.add(new Alphabet("b"));
+
+        automata.entity.dfa.Transitions expectedTransitions = new automata.entity.dfa.Transitions();
+        expectedTransitions.put(new State("q2"), new HashMap<Alphabet, State>(){
+            {put(new Alphabet("b"), new State("q3"));}
+            {put(new Alphabet("a"), new State("q2,q3"));}
+        });
+        expectedTransitions.put(new State("q3"), new HashMap<Alphabet, State>(){
+            {put(new Alphabet("b"), new State(""));}
+            {put(new Alphabet("a"), new State("q1,q3"));}
+        });
+        expectedTransitions.put(new State(""), new HashMap<Alphabet, State>(){
+            {put(new Alphabet("b"), new State(""));}
+            {put(new Alphabet("a"), new State(""));}
+        });
+        expectedTransitions.put(new State("q1,q3"), new HashMap<Alphabet, State>(){
+            {put(new Alphabet("b"), new State("q2"));}
+            {put(new Alphabet("a"), new State("q1,q3"));}
+        });
+        expectedTransitions.put(new State("q2,q3"), new HashMap<Alphabet, State>(){
+            {put(new Alphabet("b"), new State("q3"));}
+            {put(new Alphabet("a"), new State("q1,q2,q3"));}
+        });
+        expectedTransitions.put(new State("q1,q2,q3"), new HashMap<Alphabet, State>(){
+            {put(new Alphabet("b"), new State("q2,q3"));}
+            {put(new Alphabet("a"), new State("q1,q2,q3"));}
+        });
+
+        State expectedInitialState = new State("q1,q3");
+
+        DFA expectedDFA = new DFA(expectedStates, expectedAlphabets, expectedTransitions, expectedInitialState, expectedFinalStates);
+
+        assertThat(dfa, is(expectedDFA));
+
+    }
 }
